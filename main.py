a564e393930b416ee80c9e068978092a2c3b9772
Cole Sashkin <csashceh1024@gmail.com>
2025-09-24 10:59:36 -0700

    update

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import sdcardio
 import storage
 from adafruit_st7735r import ST7735R
 import ulab
+import struct
 
 # SD card pins
 sd_mosi_pin = board.GP19
@@ -72,6 +73,7 @@ cam = OV7670(
 )
 cam.size =  cam_size
 cam.flip_y = True
+
 def create_palette(bm):
     colors = []
     for x in range(bm.width):
@@ -84,6 +86,22 @@ def create_palette(bm):
         palette[i] = color
     return palette
 
+def save_as_bmp(filename, width, height, pixel_data):
+    with open(filename, 'wb') as fh:
+        # BMP Header
+        fh.write(b'BM')
+        file_size = 54 + len(pixel_data)
+        fh.write(struct.pack('<IHHI', file_size, 0, 0, 54))
+        
+        # DIB Header
+        fh.write(struct.pack('<IiiHHIIiiII',
+            40, width, height, 1, 24, 0,
+            len(pixel_data), 2835, 2835, 0, 0
+        ))
+        
+        # Pixel Data (BGR format)
+        fh.write(pixel_data)
+
 display.auto_refresh = False
 img_idx = 0
 while True:
@@ -91,9 +109,8 @@ while True:
     camera_image.dirty()
     display.refresh(minimum_frames_per_second=0)
     # palette = create_palette(camera_image)
-    #TODO: write custom function to convert bitmap to jpg
-    buffer = ulab.numpy.frombuffer(camera_image)
-    with open(capture_file.format(img_idx), "wb") as fh:
-        fh.write(buffer)
+    # buffer = ulab.numpy.frombuffer(camera_image)
+    filename = capture_file.format(img_idx)
+    save_as_bmp(filename, camera_image.width, camera_image.height, camera_image)
     print("Saved Image {}".format(img_idx))
     img_idx += 1    
